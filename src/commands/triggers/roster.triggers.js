9703e0ac2457fb6a55f54a68008f95e60e201489
jsouf <reeb.vi@gmail.com>
2021-10-06 16:20:59 +0200

    fix(embedMessage): fix embed structure to avoid max length errors

--- src/commands/triggers/roster.triggers.js
+++ src/commands/triggers/roster.triggers.js
@@ -12,18 +12,18 @@ async function updateOrCreateMessageRoster(client, channel) {
     if (channel) {
         let messageRosterEmbed;
         const idBot = client.user.id;
-		const predicateIsMessageRoster = msg => { 
-			const isBot = msg.author.id === idBot;
-			const embed = msg.embeds ? msg.embeds[0] : '';
-			return isBot && embed && embed.title.length > 0 && embed.title.trim() === titleMessageEmbed;
-		}
+        const predicateIsMessageRoster = msg => {
+            const isBot = msg.author.id === idBot;
+            const embed = msg.embeds ? msg.embeds[0] : '';
+            return isBot && embed && embed.title.length > 0 && embed.title.trim() === titleMessageEmbed;
+        }
         const messageRoster = (await channel.messages.fetch()).find(predicateIsMessageRoster);
         if (messageRoster) {
             messageRosterEmbed = messageRoster.embeds[0];
         }
 
         const messageEmbed = await showPlayersByRole(channel);
-        
+
         if (messageRosterEmbed && messageRosterEmbed.length > 0) {
             messageRoster.edit(messageEmbed);
         }
@@ -34,28 +34,44 @@ async function updateOrCreateMessageRoster(client, channel) {
 }
 
 async function showPlayersByRole(channel) {
-    let fieldValue = '';
-    let messageEmbed;
+    let fields = [];
+    let messageEmbed = new MessageEmbed()
+        .setColor('#202225')
+        .setTitle(titleMessageEmbed);
+
     for (const key of Object.keys(roles)) {
         const role = roles[key];
         let roleDiscord = await channel.guild.roles.fetch(role.id);
         if (roleDiscord) {
-            let players = 'aucun joueur';
             if (roleDiscord.members && roleDiscord.members.size > 0) {
-                players = roleDiscord.members.map(member => `${member}`).sort().join(' | ');
+                const members = roleDiscord.members.sort();
+                let iterationMember = 0;
+                let roleValue = `\n \u200B \n ${roleDiscord} \n\n`;
+                let firstFieldExist = false;
+                let fieldValue = '';
+
+                for (const member of members) {
+                    fieldValue += `<@${member[0]}>\n`;
+                    if (iterationMember === 5) {
+                        fields.push({ name: '_', value: !firstFieldExist ? roleValue + fieldValue : fieldValue, inline: firstFieldExist });
+                        firstFieldExist = true;
+                        iterationMember = 0;
+                    }
+                    iterationMember++;
+                }
+
+                if(iterationMember < 5) {
+                    fields.push({ name: '_', value: !firstFieldExist ? roleValue + fieldValue : fieldValue });
+                }
             }
-            fieldValue += `\n \u200B \n ${roleDiscord} \n \n ${players}`;
+            else {
+                fields.push({ name: `${roleDiscord}`, value: 'aucun joueur' });
+            }
         }
     }
 
-    if (fieldValue.length > 0) {
-        messageEmbed = new MessageEmbed()
-            .setColor('#202225')
-            .setTitle(titleMessageEmbed)
-            .addField(
-                'Liste des joueurs :',
-                fieldValue
-            );
+    if (fields.length > 0) {
+        messageEmbed.addFields(fields);
     }
 
     return messageEmbed;

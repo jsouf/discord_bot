0dea76042b15ef1a61cad90ec1b004aef9cf9dd9
jsouf <reeb.vi@gmail.com>
2021-10-07 20:29:19 +0200

    feat(territories): add Weaver's Fen

--- src/enums/territories.enums.js
+++ src/enums/territories.enums.js
@@ -5,6 +5,7 @@ const territories =  [
     { id: 4, name: "Monarch's Bluffs", alias: "mb" },
     { id: 5, name: "Mourningdale", alias: "md" },
     { id: 6, name: "Reekwater", alias: "rw" },
+    { id: 8, name: "Weaver's Fen", alias: "wf" },
     { id: 7, name: "Windsward", alias: "ws" }
 ];
 

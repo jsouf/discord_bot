c5b8a6b20508959105e95b14f1a4e48bf84586bd
jsouf <reeb.vi@gmail.com>
2021-10-08 02:07:39 +0200

    feat(territories): add Ebonscale Reach

--- src/enums/territories.enums.js
+++ src/enums/territories.enums.js
@@ -1,12 +1,13 @@
 const territories =  [
     { id: 1, name: "Brightwood", alias: "bw" },
-    { id: 2, name: "Everfall", alias: "ef" },
-    { id: 3, name: "First Light", alias: "fl" },
-    { id: 4, name: "Monarch's Bluffs", alias: "mb" },
-    { id: 5, name: "Mourningdale", alias: "md" },
-    { id: 6, name: "Reekwater", alias: "rw" },
+    { id: 2, name: "Ebonscale Reach", alias: "er" },
+    { id: 3, name: "Everfall", alias: "ef" },
+    { id: 4, name: "First Light", alias: "fl" },
+    { id: 5, name: "Monarch's Bluffs", alias: "mb" },
+    { id: 6, name: "Mourningdale", alias: "md" },
+    { id: 7, name: "Reekwater", alias: "rw" },
     { id: 8, name: "Weaver's Fen", alias: "wf" },
-    { id: 7, name: "Windsward", alias: "ws" }
+    { id: 9, name: "Windsward", alias: "ws" }
 ];
 
 module.exports = territories;
